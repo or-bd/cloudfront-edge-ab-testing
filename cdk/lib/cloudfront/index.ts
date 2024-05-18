1a7d8201c3da09ff37e1ca019e339fdf0e0cf449
Or Ben Dahan <or.ben-dahan@risecodes.com>
2024-05-18 14:40:40 +0300

    custom policy with max age zero added to testing.js file

--- cdk/lib/cloudfront/index.ts
+++ cdk/lib/cloudfront/index.ts
@@ -6,79 +6,95 @@ import * as cfOrigins from 'aws-cdk-lib/aws-cloudfront-origins';
 import { CfnOutput, RemovalPolicy } from 'aws-cdk-lib';
 
 interface IProps {
-    bucket: s3.IBucket,
-    path: string,
-    oai: cf.IOriginAccessIdentity,
+  bucket: s3.IBucket,
+  path: string,
+  oai: cf.IOriginAccessIdentity,
 }
 
 export class Cloudfront extends Construct {
-    public id: string;
-    public distribution: cf.Distribution;
+  public id: string;
+  public distribution: cf.Distribution;
 
-    constructor(scope: Construct, id: string, props: IProps) {
-        super(scope, id);
+  constructor(scope: Construct, id: string, props: IProps) {
+    super(scope, id);
 
-        this.id = id;
+    this.id = id;
 
-        const origin = new cfOrigins.S3Origin(props.bucket, {
-            originAccessIdentity: props.oai,
-            originPath: `/${props.path}`
-        });
+    const origin = new cfOrigins.S3Origin(props.bucket, {
+      originAccessIdentity: props.oai,
+      originPath: `/${props.path}`
+    });
 
-        this.distribution = new cf.Distribution(this, `${id}-distribution`, {
-            comment: id,
-            defaultRootObject: 'index.html',
-            defaultBehavior: {
-                origin,
-                compress: true,
-                cachePolicy: cf.CachePolicy.CACHING_OPTIMIZED,
-                viewerProtocolPolicy: cf.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-            },
-            errorResponses: [
-                { httpStatus: 404, responseHttpStatus: 200, responsePagePath: '/index.html', },
-            ],
-        });
+    this.distribution = new cf.Distribution(this, `${id}-distribution`, {
+      comment: id,
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        origin,
+        compress: true,
+        cachePolicy: cf.CachePolicy.CACHING_OPTIMIZED,
+        viewerProtocolPolicy: cf.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      errorResponses: [
+        { httpStatus: 404, responseHttpStatus: 200, responsePagePath: '/index.html', },
+      ],
+    });
 
-        this.distribution.addBehavior('/testing.js', origin, {
-            compress: true,
-            cachePolicy: cf.CachePolicy.CACHING_DISABLED,
-            viewerProtocolPolicy: cf.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-            functionAssociations: [{
-                function: this.createEdgeFunction(),
-                eventType: cf.FunctionEventType.VIEWER_REQUEST,
-            }],
-        });
+    this.distribution.addBehavior('/testing.js', origin, {
+      compress: true,
+      cachePolicy: cf.CachePolicy.CACHING_OPTIMIZED,
+      responseHeadersPolicy: this.createNoCachePolicy(),
+      viewerProtocolPolicy: cf.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      functionAssociations: [{
+        function: this.createEdgeFunction(),
+        eventType: cf.FunctionEventType.VIEWER_REQUEST,
+      }],
+    });
 
-        new CfnOutput(this, `distribution-${id}-url`, { value: `https://${this.distribution.domainName}`, });
-    }
+    new CfnOutput(this, `distribution-${id}-url`, { value: `https://${this.distribution.domainName}`, });
+  }
 
-    public createKeyValueStore() {
-        const comment = `${this.id}-store`;
+  public createNoCachePolicy() {
+    return new cf.ResponseHeadersPolicy(this, `${this.id}-no-cache-policy`, {
+      comment: 'cache control policy with max-age zero',
+      customHeadersBehavior: {
+        customHeaders: [
+          {
+            header: 'Cache-Control',
+            value: 'max-age=0',
+            override: true
+          }
+        ]
+      }
+    });
+  }
 
-        return new cf.KeyValueStore(this, `${this.id}-key-value-store`, {
-            keyValueStoreName: `${comment}-${new Date().getTime()}`,
-            source: cf.ImportSource.fromAsset('./lib/cloudfront/store.json'),
-            comment,
-        });
-    }
+  public createKeyValueStore() {
+    const comment = `${this.id}-store`;
 
-    public createEdgeFunction() {
-        const store = this.createKeyValueStore();
-        let code = fs.readFileSync('./lib/cloudfront/function.js', 'utf-8').toString();
-        code = code.replace('{{keyValueStoreId}}', store.keyValueStoreId);
+    return new cf.KeyValueStore(this, `${this.id}-key-value-store`, {
+      keyValueStoreName: `${comment}-${new Date().getTime()}`,
+      source: cf.ImportSource.fromAsset('./lib/cloudfront/store.json'),
+      comment,
+    });
+  }
 
-        return new cf.Function(this, `${this.id}-function`, {
-            comment: `${this.id}-function`,
-            keyValueStore: {
-                stack: store.stack,
-                env: store.env,
-                node: store.node,
-                keyValueStoreArn: store.keyValueStoreArn,
-                keyValueStoreId: store.keyValueStoreId,
-                keyValueStoreStatus: store.keyValueStoreStatus,
-                applyRemovalPolicy: () => store.applyRemovalPolicy(RemovalPolicy.DESTROY)
-            },
-            code: cf.FunctionCode.fromInline(code)
-        });
-    }
+  public createEdgeFunction() {
+    const store = this.createKeyValueStore();
+    let code = fs.readFileSync('./lib/cloudfront/function.js', 'utf-8').toString();
+    code = code.replace('{{keyValueStoreId}}', store.keyValueStoreId);
+
+    return new cf.Function(this, `${this.id}-function`, {
+      comment: `${this.id}-function`,
+      keyValueStore: {
+        stack: store.stack,
+        env: store.env,
+        node: store.node,
+        keyValueStoreArn: store.keyValueStoreArn,
+        keyValueStoreId: store.keyValueStoreId,
+        keyValueStoreStatus: store.keyValueStoreStatus,
+        applyRemovalPolicy: () => store.applyRemovalPolicy(RemovalPolicy.DESTROY)
+      },
+      code: cf.FunctionCode.fromInline(code)
+    });
+  }
 }
